9d3bc86e758b28da1576ce1a14fe8599e6a1d5fa
niha8515 <niha8515@yahoo.com>
2022-04-18 15:44:53 +0430

    add WordRepository.kt

---
+++ app/src/main/java/com/example/dictionary/repository/WordRepository.kt
@@ -0,0 +1,35 @@
+package com.example.dictionary.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.example.dictionary.database.AppDatabase
+import com.example.dictionary.database.Word
+import com.example.dictionary.database.WordDao
+import java.lang.Appendable
+
+object WordRepository {
+
+    private var db : AppDatabase? = null
+    private var wordDao : WordDao? = null
+
+    fun initDB(context: Context){
+        db = AppDatabase.getAppDataBase(context)
+        wordDao = db?.wordDao()
+    }
+
+    fun findFa (fa :String): Word? {
+        return wordDao?.findFaWord(fa)
+    }
+    fun findEn (en :String): Word? {
+        return wordDao?.findEnWord(en)
+    }
+    fun insert (word: Word){
+        wordDao?.insert(word)
+    }
+    fun countWords (): LiveData<Int>? {
+        return wordDao?.countAllWords()
+    }
+    fun delete (word: Word) {
+        wordDao?.delete(word)
+    }
+}
