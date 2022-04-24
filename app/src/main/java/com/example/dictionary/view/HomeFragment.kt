3a5f688067b2690cec03620e3a076f6ecddd1475
niha8515 <niha8515@yahoo.com>
2022-04-24 10:35:27 +0430

    add click listener to fab in HomeFragment.kt

--- app/src/main/java/com/example/dictionary/view/HomeFragment.kt
+++ app/src/main/java/com/example/dictionary/view/HomeFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.dictionary.R
+import com.example.dictionary.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +23,16 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddWord.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addWordFragment)
+        }
     }
 
 }
