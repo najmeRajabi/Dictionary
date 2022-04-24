4839eea98dd8a6d3eed38824a2f0f06f81736bfd
niha8515 <niha8515@yahoo.com>
2022-04-24 12:40:48 +0430

    update WordAdaptor.kt

--- app/src/main/java/com/example/dictionary/adaptor/WordAdaptor.kt
+++ app/src/main/java/com/example/dictionary/adaptor/WordAdaptor.kt
@@ -1,24 +1,32 @@
 package com.example.dictionary.adaptor
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dictionary.R
 import com.example.dictionary.database.Word
 
-class WordAdaptor(val dataset: ArrayList<Word>):
+class WordAdaptor(val dataSet: ArrayList<Word>):
                   RecyclerView.Adapter<WordAdaptor.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordAdaptor.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.word_item_row, parent, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: WordAdaptor.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.txvEnglish.text = dataSet[position].en
+        holder.txvFarsi.text = dataSet[position].fa
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount() = dataSet.size
+
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val txvEnglish = view.findViewById<TextView>(R.id.txv_en_row)
+        val txvFarsi = view.findViewById<TextView>(R.id.txv_fa_row)
     }
-
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view){}
 }
