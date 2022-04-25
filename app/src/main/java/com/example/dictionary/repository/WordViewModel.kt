5dcbb434a70138f01cf5857fef615157d0612a3f
niha8515 <niha8515@yahoo.com>
2022-04-25 12:28:06 +0430

    add fun from repository

--- app/src/main/java/com/example/dictionary/repository/WordViewModel.kt
+++ app/src/main/java/com/example/dictionary/repository/WordViewModel.kt
@@ -2,6 +2,36 @@ package com.example.dictionary.repository
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.dictionary.database.Word
 
 class WordViewModel(app: Application):AndroidViewModel(app) {
+
+
+    init {
+        WordRepository.initDB(app)
+    }
+
+    fun getAll(): LiveData<List<Word>>? {
+        return WordRepository.getAll()
+    }
+
+    fun findFa (fa :String): Word? {
+        return WordRepository.findFa(fa)
+    }
+    fun findEn (en :String): Word? {
+        return WordRepository.findEn(en)
+    }
+    fun insert (word: Word){
+        WordRepository.insert(word)
+    }
+    fun countWords (): LiveData<Int>? {
+        return WordRepository.countWords()
+    }
+    fun delete (word: Word) {
+        WordRepository.delete(word)
+    }
+    fun update (word: Word){
+        WordRepository.update(word)
+    }
 }
