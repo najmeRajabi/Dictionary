bc04840b4edd8e743b59a31ed77dab32ed003e29
niha8515 <niha8515@yahoo.com>
2022-04-25 11:50:32 +0430

    add WordViewModel.kt

---
+++ app/src/main/java/com/example/dictionary/repository/WordViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dictionary.repository
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WordViewModel(app: Application):AndroidViewModel(app) {
+}
