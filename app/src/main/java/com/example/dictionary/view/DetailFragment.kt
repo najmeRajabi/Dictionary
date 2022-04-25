bf024994495cc3ea2b133b5a73e01683fd88a486
niha8515 <niha8515@yahoo.com>
2022-04-25 16:57:07 +0430

    receive word id in DetailFragment.kt and set in view

--- app/src/main/java/com/example/dictionary/view/DetailFragment.kt
+++ app/src/main/java/com/example/dictionary/view/DetailFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import com.example.dictionary.R
+import com.example.dictionary.database.Word
+import com.example.dictionary.databinding.FragmentDetailBinding
+import com.example.dictionary.repository.WordViewModel
 
 class DetailFragment : Fragment() {
 
+    val args: DetailFragmentArgs by navArgs()
+    lateinit var binding: FragmentDetailBinding
+    val vModel : WordViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +27,24 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val id = args.wordArg.minus(1)
+        var word :Word
+        vModel.wordList?.observe(requireActivity()){
+            word = it[id]
+            binding.txvEn.text = word.en
+            binding.txvFa.text = word.fa
+            binding.txvExample.text = word.example
+            binding.txvSynonyms.text = word.synonyms
+        }
+
     }
 
 }
