1fd38c370a0af03ec512c11525fe16350c9967c3
niha8515 <niha8515@yahoo.com>
2022-04-25 12:50:03 +0430

    update recycler view by view model in HomeFragment.kt

--- app/src/main/java/com/example/dictionary/view/HomeFragment.kt
+++ app/src/main/java/com/example/dictionary/view/HomeFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.dictionary.R
 import com.example.dictionary.adaptor.WordAdaptor
 import com.example.dictionary.database.Word
 import com.example.dictionary.databinding.FragmentHomeBinding
+import com.example.dictionary.repository.WordViewModel
 
 class HomeFragment : Fragment() {
 
     lateinit var binding : FragmentHomeBinding
+    val vModel: WordViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,24 +44,17 @@ class HomeFragment : Fragment() {
 
     private fun initViews() {
 
-        val wordList= arrayListOf(
-            Word(1,"fa 1", "en 1" , "synon 1", "example 1"),
-            Word(2,"fa 2", "en 2" , "synon 2", "example 2"),
-            Word(3,"fa 3", "en 3" , "synon 3", "example 3"),
-            Word(4,"fa 4", "en 4" , "synon 4", "example 4"),
-            Word(5,"fa 5", "en 5" , "synon 5", "example 5"),
-            Word(6,"fa 6", "en 6" , "synon 6", "example 6")
-        )
-
         val recyclerViewWord = binding.recyclerWord
         val adapter = WordAdaptor(arrayListOf()){
             word ->
             findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
         }
-        adapter.submitList(wordList)
+
+        vModel.wordList?.observe(requireActivity()){
+            adapter.submitList(it)
+        }
+
         recyclerViewWord?.adapter = adapter
-//        adapter.dataSet= wordList
-//        adapter.notifyDataSetChanged()
     }
 
 }
