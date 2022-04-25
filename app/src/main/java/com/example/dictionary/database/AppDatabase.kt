de342e296ffc77a485133dd65b67775c169b660e
niha8515 <niha8515@yahoo.com>
2022-04-25 12:20:22 +0430

    update AppDatabase.kt

--- app/src/main/java/com/example/dictionary/database/AppDatabase.kt
+++ app/src/main/java/com/example/dictionary/database/AppDatabase.kt
@@ -5,28 +5,32 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Word::class] , version = 1)
+@Database(entities = [Word::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun wordDao() :WordDao
+    abstract fun wordDao(): WordDao
 
     companion object {
+        @Volatile
         var INSTANCE: AppDatabase? = null
 
         fun getAppDataBase(context: Context): AppDatabase? {
-            if (INSTANCE == null){
-                synchronized(AppDatabase::class){
-                    INSTANCE =
-                        Room.databaseBuilder(context.applicationContext,
-                            AppDatabase::class.java, "myDB")
-                            .allowMainThreadQueries()
-                            .build()
-                }
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
             }
-            return INSTANCE
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
         }
 
-        fun destroyDataBase(){
+        fun destroyDataBase() {
             INSTANCE = null
         }
     }
