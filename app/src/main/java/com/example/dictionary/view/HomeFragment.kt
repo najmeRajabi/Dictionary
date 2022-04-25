e028f11f138a9eee2846b118c498b9bd4368f563
niha8515 <niha8515@yahoo.com>
2022-04-25 10:48:03 +0430

    init views in HomeFragment.kt adapter , recyclerView

--- app/src/main/java/com/example/dictionary/view/HomeFragment.kt
+++ app/src/main/java/com/example/dictionary/view/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.dictionary.R
+import com.example.dictionary.adaptor.WordAdaptor
+import com.example.dictionary.database.Word
 import com.example.dictionary.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -30,9 +32,33 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
+
         binding.fabAddWord.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addWordFragment)
         }
+    }
+
+    private fun initViews() {
+
+        val wordList= arrayListOf(
+            Word(1,"fa 1", "en 1" , "synon 1", "example 1"),
+            Word(2,"fa 2", "en 2" , "synon 2", "example 2"),
+            Word(3,"fa 3", "en 3" , "synon 3", "example 3"),
+            Word(4,"fa 4", "en 4" , "synon 4", "example 4"),
+            Word(5,"fa 5", "en 5" , "synon 5", "example 5"),
+            Word(6,"fa 6", "en 6" , "synon 6", "example 6")
+        )
+
+        val recyclerViewWord = binding.recyclerWord
+        val adapter = WordAdaptor(arrayListOf()){
+            word ->
+            findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
+        }
+        adapter.submitList(wordList)
+        recyclerViewWord?.adapter = adapter
+//        adapter.dataSet= wordList
+//        adapter.notifyDataSetChanged()
     }
 
 }
